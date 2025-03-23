6c3cda6b49ea719c4223c0cc3f92ab454f245e74
ArturoMD7 <arturomtzdel17@gmail.com>
2025-03-23 16:38:13 -0600

    Publicaciones

--- post/serializers.py
+++ post/serializers.py
@@ -1,9 +1,23 @@
 from rest_framework import serializers
 from .models import Post
+from user.models import UsersProfile  # Importación corregida
 
 class PostSerializer(serializers.ModelSerializer):
+    user_name = serializers.SerializerMethodField()
+    user_profile_photo = serializers.SerializerMethodField()
+
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = ['id', 'title', 'description', 'postDate', 'petId', 'userId', 'user_name', 'user_profile_photo']
 
-        
+    def get_user_name(self, obj):
+        return obj.userId.name  # Suponiendo que el modelo User tiene un campo 'name'
+
+    def get_user_profile_photo(self, obj):
+        try:
+            profile = UsersProfile.objects.get(user=obj.userId)
+            if profile.profilePhoto:
+                return self.context['request'].build_absolute_uri(profile.profilePhoto.url)
+        except UsersProfile.DoesNotExist:
+            pass
+        return None
--- usersprofile/models.py
+++ usersprofile/models.py
@@ -5,8 +5,8 @@ from user.models import User
 
 class UsersProfile(models.Model):
     description = models.TextField(blank=True, null=True)
-    profilePhotoURL = models.CharField(max_length=254, blank=True, null=True)
+    profilePhoto = models.ImageField(upload_to='profile_photos/', blank=True, null=True)
     state = models.CharField(max_length=30)
     city = models.CharField(max_length=30)
     address = models.CharField(max_length=254)
-    userId = models.ForeignKey(User, on_delete=models.CASCADE)
+    userId = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile', unique=True)  # Asegurar que sea único
